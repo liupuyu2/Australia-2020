c1900e534cf2fedd7cceae6fd0367d1a6a14dcc4
liupuyu2 <puyu.liu@mail.utoronto.ca>
2022-03-16 23:31:09 +0000

    rmd, data prep

---
+++ scripts/01-data_preparation.R
@@ -0,0 +1,27 @@
+#### Preamble ####
+# Purpose: Prepare the 2020 Australia survey data downloaded from 
+# Author: Yuxuan Yang, Puyu Liu, Fengyuan Tang
+# Data: 13 March 2022
+# Contact: yuxuanmaggie.yang@utoronto.ca 
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+
+
+#### Workspace setup ####
+# Use R Projects, not setwd().
+library(haven)
+library(tidyverse)
+library(readxl)
+
+# Read in the raw data. 
+raw_data <- readxl::read_excel("inputs/data/GSS_Table8.xlsx", sheet = "Table 8.1_Mean Score")
+# Just keep some variables that may be of interest (change 
+# this depending on your interests)
+
+raw_data_2 <- readxl::read_excel("inputs/data/GSS_Table8.xlsx", sheet = "Table 8.2_RSE, mean")
+#### What's next? ####
+
+
+
+         
